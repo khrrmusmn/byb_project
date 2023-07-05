684a730761ba4167be9532e5402c4ef146a8946e
Khurram Usman <khrrmusmn@gmail.com>
2023-07-05 14:19:10 +0100

    last commit

--- helloWorld.py
+++ helloWorld.py
@@ -1,5 +1,8 @@
-# demo input the desire word "Hello World!" and get printed
-# input = input("enter an input:")
-# print("entered input:", input)
+# demo input the desire word "Git is Awesome!" and get printed
+input = input("enter an input:")
+# this method is helpful if we need strings around 'Git is Awesome!'
+print("entered input:", input)
+
+# Alternativey, we should just need to run simple print function 
 print("Git is Awesome!")
     
